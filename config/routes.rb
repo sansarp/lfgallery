17b9ec34b38f0c3e6e834d90fb1e1826e6f98ade
spoudyal <subashpoudyal@lftechnology.com>
2013-08-02 10:51:25 +0545

    path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Gallery::Application.routes.draw do
   resources :albums
 
-  get "users/index"
-  get "users/show"
+  root 'albums#index'
+
+  # get "users/index"
+  # get "users/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
